99d3dfc9fc3964683779483108aefc42ac7c9e62
Severin Spoerri <severin.spoerri@outlook.com>
2023-10-24 23:18:38 +0200

    unicode map in config.h

--- config.h
+++ config.h
@@ -1,1 +1,13 @@
-#define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE, UNICODE_MODE_LINUX
+#define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
+
+enum unicode_names {
+    BANG,
+    IRONY,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [BANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+};
--- rules.mk
+++ rules.mk
@@ -1,3 +1,4 @@
 SRC += source.c
 
 UNICODE_ENABLE = yes
+UNICODEMAP_ENABLE = yes
