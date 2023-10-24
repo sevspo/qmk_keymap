1c3509804ba8cc813c4b0aad8828c2812dec6234
Severin Spoerri <severin.spoerri@outlook.com>
2023-10-24 23:25:34 +0200

    enum in source.c

--- config.h
+++ config.h
@@ -1,13 +1,1 @@
 #define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
-
-enum unicode_names {
-    BANG,
-    IRONY,
-    SNEK
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [BANG]  = 0x203D,  // ‽
-    [IRONY] = 0x2E2E,  // ⸮
-    [SNEK]  = 0x1F40D, // 🐍
-};
--- source.c
+++ source.c
@@ -1,1 +1,13 @@
 #include QMK_KEYBOARD_H
+
+enum unicode_names {
+    BANG,
+    IRONY,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [BANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+};
